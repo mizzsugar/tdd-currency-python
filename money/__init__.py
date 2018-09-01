f9108bd938b183ac18a9eadbb374a8c2f626bc02
Mizuki Sato <mizki.sato.0025@gmail.com>
2018-09-01 22:58:48 +0900

    Made Franc class

--- money/__init__.py
+++ money/__init__.py
@@ -1,9 +1,20 @@
 class Dollar:
     def __init__(self, amount: int) -> None:
         self._amount = amount
-    
-    def __eq__(self, dollar: 'Dollar') -> bool: 
+
+    def __eq__(self, dollar: 'Dollar') -> bool:
         return self._amount == dollar._amount
 
-    def times(self, multiplier: int) -> 'Dollar': 
+    def times(self, multiplier: int) -> 'Dollar':
         return Dollar(self._amount * multiplier)
+
+
+class Franc:
+    def __init__(self, amount: int) -> None:
+        self._amount = amount
+
+    def __eq__(self, franc: 'Franc') -> bool:
+        return self._amount == franc._amount
+
+    def times(self, multiplier: int) -> 'Franc':
+        return Franc(self._amount * multiplier)
