ef9d6a97163a410c51cb33ac64a4f8921c434413
Mizuki Sato <mizki.sato.0025@gmail.com>
2018-09-01 19:35:49 +0900

    Times method returns dollar instance.

--- money/__init__.py
+++ money/__init__.py
@@ -2,5 +2,5 @@ class Dollar:
     def __init__(self, amount: int) -> None:
         self.amount = amount
 
-    def times(self, multiplier: int) -> None:
-        self.amount *= multiplier
+    def times(self, multiplier: int) -> 'Dollar': 
+        return Dollar(self.amount * multiplier)
