d0904c65e168b6b7c80d95bc4d5ed94b45280b14
Mizuki Sato <mizki.sato.0025@gmail.com>
2018-09-01 21:52:33 +0900

    Dollar object is comparable.

--- money/__init__.py
+++ money/__init__.py
@@ -1,6 +1,9 @@
 class Dollar:
     def __init__(self, amount: int) -> None:
         self.amount = amount
+    
+    def __eq__(self, dollar: 'Dollar') -> bool: 
+        return self.amount == dollar.amount
 
     def times(self, multiplier: int) -> 'Dollar': 
         return Dollar(self.amount * multiplier)
