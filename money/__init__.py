dea67e111ab22e0c6d658343616196b039d2e77d
mizuki sato <cbclcscpmsaycc@gmail.com>
2018-09-08 16:39:56 +0900

    Franc also extends Money.

--- money/__init__.py
+++ money/__init__.py
@@ -11,12 +11,9 @@ class Dollar(Money):
         return Dollar(self._amount * multiplier)
 
 
-class Franc:
+class Franc(Money):
     def __init__(self, amount: int) -> None:
         self._amount = amount
-
-    def __eq__(self, franc: 'Franc') -> bool:
-        return self._amount == franc._amount
 
     def times(self, multiplier: int) -> 'Franc':
         return Franc(self._amount * multiplier)
