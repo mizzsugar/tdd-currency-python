7ff1b16392c420a0c69b492b5ecf8c89b5d752af
mizuki sato <cbclcscpmsaycc@gmail.com>
2018-09-08 19:31:45 +0900

    init in Dollar and Franc extends Money's init.

--- money/__init__.py
+++ money/__init__.py
@@ -2,6 +2,10 @@ import abc
 
 
 class Money(abc.ABC):
+    def __init__(self, amount: int, currency: str) -> None:
+        self._amount = amount
+        self._currency = currency
+
     def __eq__(self, money: 'Money') -> bool:
         return (
             self._amount == money._amount and
@@ -20,28 +24,21 @@ class Money(abc.ABC):
     def times(self, multiplier: int) -> 'Money':
         pass
 
-    @abc.abstractclassmethod
     def currency(self) -> str:
-        pass
+        return self._currency
 
 
 class Dollar(Money):
     def __init__(self, amount: int) -> None:
-        self._amount = amount
+        super().__init__(amount, 'USD')
 
     def times(self, multiplier: int) -> 'Money':
         return Dollar(self._amount * multiplier)
 
-    def currency(self) -> str:
-        return "USD"
-
 
 class Franc(Money):
     def __init__(self, amount: int) -> None:
-        self._amount = amount
+        super().__init__(amount, 'CHF')
 
     def times(self, multiplier: int) -> 'Money':
         return Franc(self._amount * multiplier)
-
-    def currency(self) -> str:
-        return "CHF"
