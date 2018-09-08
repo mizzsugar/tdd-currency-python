96740b878dc409e0b7738476b5b52cd807ce86cf
mizuki sato <cbclcscpmsaycc@gmail.com>
2018-09-08 16:30:43 +0900

    Implement Money class and set __eq__ function in it.

--- money/__init__.py
+++ money/__init__.py
@@ -1,9 +1,11 @@
-class Dollar:
+class Money:
+    def __eq__(self, money: 'Money') -> bool:
+        return self._amount == money._amount
+
+
+class Dollar(Money):
     def __init__(self, amount: int) -> None:
         self._amount = amount
-
-    def __eq__(self, dollar: 'Dollar') -> bool:
-        return self._amount == dollar._amount
 
     def times(self, multiplier: int) -> 'Dollar':
         return Dollar(self._amount * multiplier)
