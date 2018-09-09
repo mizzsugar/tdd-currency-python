7b2b2eab468659d320c67b283593953a2a467d2d
Motoki Naruse <motoki@naru.se>
2018-09-09 18:05:46 +0900

    Give currency to reduce method

--- tests/test_money.py
+++ tests/test_money.py
@@ -22,7 +22,7 @@ class TestMoney:
         assert money.Money.franc(5) != five_dollar
 
     def test_simple_addition(self, five_dollar):
-        reduced = money.Bank().reduce(five_dollar.plus(five_dollar))
+        reduced = money.Bank().reduce(five_dollar.plus(five_dollar), "USD")
 
         assert reduced == money.Money.dollar(10)
 
@@ -35,7 +35,7 @@ class TestMoney:
     def test_reduce_sum(self):
         sum_expression = money.SumExpression(
             money.Money.dollar(3), money.Money.dollar(4))
-        reduced = money.Bank().reduce(sum_expression)
+        reduced = money.Bank().reduce(sum_expression, "USD")
         assert reduced == money.Money.dollar(7)
 
     def test_reduce_to_different_currency(self):
