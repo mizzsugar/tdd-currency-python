f25639793f2e47ed170649e166d1de440c8810f4
Motoki Naruse <motoki@naru.se>
2018-09-09 16:05:54 +0900

    Refactoring to use fixture for 5 dollar

--- tests/test_money.py
+++ tests/test_money.py
@@ -8,23 +8,21 @@ class TestMoney:
     def five_dollar(self):
         return money.Money.dollar(5)
 
-    def test_currency(self):
-        assert "USD" == money.Money.dollar(1).currency()
+    def test_currency(self, five_dollar):
+        assert "USD" == five_dollar.currency()
         assert "CHF" == money.Money.franc(1).currency()
 
-    def test_multiplication(self):
-        five = money.Money.dollar(5)
-        assert money.Money.dollar(10) == five.times(2)
-        assert money.Money.dollar(15) == five.times(3)
+    def test_multiplication(self, five_dollar):
+        assert money.Money.dollar(10) == five_dollar.times(2)
+        assert money.Money.dollar(15) == five_dollar.times(3)
 
-    def test_equality(self):
-        assert money.Money.dollar(5) == money.Money.dollar(5)
+    def test_equality(self, five_dollar):
+        assert five_dollar == five_dollar
         assert money.Money.franc(5) != money.Money.franc(6)
-        assert money.Money.franc(5) != money.Money.dollar(5)
+        assert money.Money.franc(5) != five_dollar
 
-    def test_simple_addition(self):
-        five = money.Money.dollar(5)
-        reduced = money.Bank().reduce(five.plus(five))
+    def test_simple_addition(self, five_dollar):
+        reduced = money.Bank().reduce(five_dollar.plus(five_dollar))
 
         assert reduced == money.Money.dollar(10)
 
